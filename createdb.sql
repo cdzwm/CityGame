804b33b68cdbfc4fcadf5436f55d7811332f796f
canw <nodejs@live.cn>
2012-08-24 14:12:19 +0800

    Add not null property

--- createdb.sql
+++ createdb.sql
@@ -7,16 +7,16 @@ create unique index idx_tbl_name on object(tbl_name);
 create table if not exists resource_type(id int not null, name varchar(64));
 create unique index idx_resource_type_id on resource_type(id);
 
-create table if not exists land( id int, x int, y int, width int, height int);
+create table if not exists land( id int not null, x int, y int, width int, height int);
 create unique index idx_land_id on land( id);
 
-create table if not exists city_resource( id int, resource_type_id int, resource_amount int);
+create table if not exists city_resource( id int not null, resource_type_id int, resource_amount int);
 create unique index idx_city_resource_id on city_resource(id);
 
-create table if not exists player(id int, name varchar(64), capital_city_id int);
+create table if not exists player(id int not null, name varchar(64), capital_city_id int);
 create unique index idx_player_id on player(id);
 
-create table if not exists city( id int, player_id int, name varchar(64), food int, gold int, tax_rate float, captil_flag int, land_id int);
+create table if not exists city( id int not null, player_id int, name varchar(64), food int, gold int, tax_rate float, captil_flag int, land_id int);
 create unique index idx_city_id on city(id);
 
 insert into object values("resource_type", 1);
