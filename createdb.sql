f33c633ad3f9cfeb02518ea6a70701e386096e93
canw <nodejs@live.cn>
2012-08-24 12:42:17 +0800

    fix database design

--- createdb.sql
+++ createdb.sql
@@ -1,26 +1,22 @@
-drop table object;
-drop table resource_type;
-drop table land;
-drop table city_resource;
-drop table player;
-drop table city;
-
-create table object(tbl_name varchar(255), object_id int);
+drop database if exists citygame;
+create database if not exists citygame;
+use citygame;
+create table if not exists object(tbl_name varchar(255), object_id int);
 create unique index idx_tbl_name on object(tbl_name); 
 
-create table resource_type(id int, name varchar(64));
+create table if not exists resource_type(id int not null, name varchar(64));
 create unique index idx_resource_type_id on resource_type(id);
 
-create table land( id int, x int, y int, width int, height int);
+create table if not exists land( id int, x int, y int, width int, height int);
 create unique index idx_land_id on land( id);
 
-create table city_resource( id int, resource_type_id int, resource_amount int);
+create table if not exists city_resource( id int, resource_type_id int, resource_amount int);
 create unique index idx_city_resource_id on city_resource(id);
 
-create table player(id int, name varchar(64), capital_city_id int);
+create table if not exists player(id int, name varchar(64), capital_city_id int);
 create unique index idx_player_id on player(id);
 
-create table city( id int, player_id int, name varchar(64), food int, gold int, tax_rate float, captil_flag int, land_id int);
+create table if not exists city( id int, player_id int, name varchar(64), food int, gold int, tax_rate float, captil_flag int, land_id int);
 create unique index idx_city_id on city(id);
 
 insert into object values("resource_type", 1);
